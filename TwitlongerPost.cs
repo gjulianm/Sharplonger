526c1069577e7f94fb75d6afbdf9e50c3c7bcda5
Guillermo Julián Moreno <gjulian93@gmail.com>
2012-10-10 20:39:38 +0200

    Initial commit

---
+++ TwitlongerPost.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Xml.Serialization;
+
+namespace Sharplonger
+{
+    [XmlRootAttribute("twitlonger")]
+    public class TwitlongerPost
+    {
+        [XmlElement("error")]
+        public string Error { get; set; }
+
+        [XmlElement("post")]
+        public TLInnerPost Post { get; set; }
+    }
+
+    [XmlRootAttribute("post")]
+    public class TLInnerPost
+    {
+        [XmlElement("id")]
+        public string Id { get; set; }
+
+        [XmlElement("link")]
+        public string Link { get; set; }
+
+        [XmlElement("short")]
+        public string ShortenedLink { get; set; }
+
+        [XmlElement("content")]
+        public string Content { get; set; }
+    }
+}
